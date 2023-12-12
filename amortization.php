7a5689db9262bf444924ee4e155a21024e1d7bd7
Blessed Jason Mwanza <mwanzabj@gmail.com>
2023-12-12 20:41:58 +0200

    Optmize and add Table header summery content

--- amortization.php
+++ amortization.php
@@ -4,11 +4,54 @@ require('./ExcelFormulas.php');
 
 class PDF extends FPDF
 {
+    private $loan_amount;
+    private $interest_rate;
+    private $loan_term;
+    private $monthly_payment;
+    private $total_interest;
+
+    function setLoanDetails($loan_amount, $interest_rate, $loan_term, $monthly_payment, $total_interest)
+    {
+        $this->loan_amount = $loan_amount;
+        $this->interest_rate = $interest_rate;
+        $this->loan_term = $loan_term;
+        $this->monthly_payment = $monthly_payment;
+        $this->total_interest = $total_interest;
+    }
+
     function header()
     {
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(0, 10, 'Amortization Schedule', 0, 1, 'C');
         $this->Ln(10);
+
+        // Summary Table
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(40, 10, 'Number of Months:', 0);
+        $this->Cell(30, 10, $this->loan_term, 0);
+        $this->Cell(40, 10, 'Interest Rate:', 0);
+        $this->Cell(30, 10, number_format($this->interest_rate * 100, 2) . '%', 0);
+        $this->Ln();
+
+        $this->Cell(40, 10, 'Principal Amount:', 0);
+        $this->Cell(30, 10, 'K' . number_format($this->loan_amount, 2), 0);
+        $this->Cell(40, 10, 'Total Interest Amount:', 0);
+        $this->Cell(30, 10, 'K' . number_format($this->total_interest, 2), 0);
+        $this->Ln();
+
+        $this->Cell(40, 10, 'Total Repayment:', 0);
+        $this->Cell(30, 10, 'K' . number_format($this->monthly_payment * $this->loan_term, 2), 0);
+        $this->Ln(15);
+
+        // Amortization Schedule Table Headers
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(20, 10, 'Month', 1);
+        $this->Cell(40, 10, 'Date', 1);
+        $this->Cell(30, 10, 'Interest', 1);
+        $this->Cell(30, 10, 'Principal', 1);
+        $this->Cell(30, 10, 'Installment', 1);
+        $this->Cell(40, 10, 'Remaining Balance', 1);
+        $this->Ln();
     }
 
     function footer()
@@ -31,24 +74,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $remaining_balance = $loan_term * $monthly_payment;
     $current_date = strtotime("now");
     $pdf = new PDF();
-    $pdf->AddPage();
+    $total_interest = 0;
 
-    $pdf->SetFont('Arial', 'B', 10);
-    $pdf->Cell(20, 10, 'Month', 1);
-    $pdf->Cell(40, 10, 'Date', 1);
-    $pdf->Cell(30, 10, 'Interest', 1);
-    $pdf->Cell(30, 10, 'Principal', 1);
-    $pdf->Cell(30, 10, 'Installment', 1);    
-    $pdf->Cell(40, 10, 'Remaining Balance', 1);
-    $pdf->Ln();
-
+    // Initial while loop to calculate total interest
     $months = 0;
-
     while ($months < $loan_term) {
         $months++;
-        $payment_date = date("jS F, Y", strtotime("+$months month", $current_date));        
-        $monthly_interest = abs(ExcelFormulas::IPMT($loan_amount, $pmt, $interest_rate, $months));
-        $monthly_principal =  abs(ExcelFormulas::PPMT($interest_rate, $months, $loan_term, $loan_amount));
+        $monthly_principal = abs(ExcelFormulas::PPMT($interest_rate, $months, $loan_term, $loan_amount));
+        $total_interest += $monthly_payment - $monthly_principal;
+    }
+
+    $pdf->setLoanDetails($loan_amount, $interest_rate, $loan_term, $monthly_payment, $total_interest);
+    $pdf->AddPage();
+
+    for ($months = 1; $months <= $loan_term; $months++) {
+        $payment_date = date("jS F, Y", strtotime("+$months month", $current_date));
+        $monthly_principal = abs(ExcelFormulas::PPMT($interest_rate, $months, $loan_term, $loan_amount));
+        $monthly_interest = $monthly_payment - $monthly_principal;
         $remaining_balance -= $monthly_payment;
 
         $pdf->Cell(20, 10, $months, 1);
